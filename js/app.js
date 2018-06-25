4de55f5a0b04714afbb14e568575d91bddf92f47
Alex Hanson <alexlhanson@gmail.com>
2018-06-25 14:13:01 -0700

    Add product constructor

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+Product.productArray = [];
+
+function Product(name, src){
+  this.name = name;
+  this.src = src;
+
+  this.likes = 0;
+  this.displayTimes = 0;
+
+  Product.productArray.push(this);
+};
+
+new Product('bag', './images/products/bag.jpg');
