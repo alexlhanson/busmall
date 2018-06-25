b644863973b07776891479db2472548eed176e7e
Alex Hanson <alexlhanson@gmail.com>
2018-06-25 14:24:00 -0700

    Add product instances

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/********************************************************************************
+*         Global variables                                                      *
+********************************************************************************/
+
+
+/********************************************************************************
+*         Product Constructor                                                   *
+********************************************************************************/
 Product.productArray = [];
 
 function Product(name, src){
@@ -12,4 +20,29 @@ function Product(name, src){
   Product.productArray.push(this);
 };
 
+/********************************************************************************
+*         Add instances and call functions for running                          *
+********************************************************************************/
+//Add Product instances
 new Product('bag', './images/products/bag.jpg');
+new Product('banana', './images/products/banana.jpg');
+new Product('bathroom', './images/products/bathroom.jpg');
+new Product('boots', './images/products/boots.jpg');
+new Product('breakfast', './images/products/breakfast.jpg');
+new Product('bubblegum', './images/products/bubblegum.jpg');
+new Product('chair', './images/products/chair.jpg');
+new Product('cthulhu', './images/products/cthulhu.jpg');
+new Product('dog-duck', './images/products/dog-duck.jpg');
+new Product('dragon', './images/products/dragon.jpg');
+new Product('pen', './images/products/pen.jpg');
+new Product('pet-sweep', './images/products/pet-sweep.jpg');
+new Product('scissors', './images/products/scissors.jpg');
+new Product('shark', './images/products/shark.jpg');
+new Product('sweep', './images/products/sweep.jpg');
+new Product('tauntaun', './images/products/tauntaun.jpg');
+new Product('unicorn', './images/products/unicorn.jpg');
+new Product('usb', './images/products/usb.gif');
+new Product('water-can', './images/products/water-can.jpg');
+new Product('wine-glass', './images/products/wine-glass.jpg');
+
+
