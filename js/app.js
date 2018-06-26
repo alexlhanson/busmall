06e3716ef524aa220cea13ed4b51c3410b1aba3e
Alex Hanson <alexlhanson@gmail.com>
2018-06-25 22:11:55 -0700

    Add event listener and handler and max counter

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@
 var product1 = document.getElementById('product1');
 var product2 = document.getElementById('product2');
 var product3 = document.getElementById('product3');
+var productForm = document.getElementById('productSelector');
+var productVoteResults = document.getElementById('productVoteResults');
 
 
 /********************************************************************************
@@ -13,13 +15,15 @@ var product3 = document.getElementById('product3');
 ********************************************************************************/
 Product.productArray = [];
 Product.lastRoundArray = [];
+Product.selectedIndexArray = [];
+Product.voteCount = 0;
 
 function Product(name, src){
   this.name = name;
   this.src = src;
 
-  this.likes = 0;
-  this.displayTimes = 0;
+  this.likesCount = 0;
+  this.displayCount = 0;
 
   Product.productArray.push(this);
 };
@@ -28,42 +32,77 @@ function Product(name, src){
 *         Product helper functions                                              *
 ********************************************************************************/
 
-Product.renderThreeImages = function(){
-  
-  //find first random number and add to page
-  do {
-    var randomNumber1 = Product.randomNumber();
-    console.log(Product.lastRoundArray.includes(randomNumber1));
-  } while (Product.lastRoundArray.includes(randomNumber1));
-
-  do {
-    var randomNumber2 = Product.randomNumber();
-    console.log(Product.lastRoundArray.includes(randomNumber2));
-  } while (randomNumber2 === randomNumber1 || Product.lastRoundArray.includes(randomNumber2));
-  
-  do{
-    var randomNumber3 = Product.randomNumber();
-    console.log(Product.lastRoundArray.includes(randomNumber1));
-  } while (randomNumber3 === randomNumber2 || randomNumber3 === randomNumber1 || Product.lastRoundArray.includes(randomNumber3));
-  
-  product1.src = Product.productArray[randomNumber1].src;
-  product2.src = Product.productArray[randomNumber2].src;
-  product3.src = Product.productArray[randomNumber3].src;
-
-  Product.lastRoundArray = [randomNumber1, randomNumber2, randomNumber3];
-  console.log(this.lastRoundArray);
-};
-
 //Create random number by Product array range
 Product.randomNumber = function(){
   var randomNumber = Math.floor(Math.random() * this.productArray.length);
   return randomNumber;
 };
 
-/********************************************************************************
-*         Event Listeners and Handlers                                          *
-********************************************************************************/
+Product.chooseThreeImages = function(){
+  //find first random number
+  do {
+    Product.indexNumber1 = Product.randomNumber();
+    console.log(Product.lastRoundArray.includes(Product.indexNumber1));
+  } while (Product.lastRoundArray.includes(Product.indexNumber1));
 
+  //find second random number
+  do {
+    Product.indexNumber2 = Product.randomNumber();
+    console.log(Product.lastRoundArray.includes(Product.indexNumber2));
+  } while (Product.indexNumber2 === Product.indexNumber1 || Product.lastRoundArray.includes(Product.indexNumber2));
+  
+  //find third random number
+  do{
+    Product.indexNumber3 = Product.randomNumber();
+    console.log(Product.lastRoundArray.includes(Product.indexNumber1));
+  } while (Product.indexNumber3 === Product.indexNumber2 || Product.indexNumber3 === Product.indexNumber1 || Product.lastRoundArray.includes(Product.indexNumber3));
+  
+  Product.lastRoundArray = [Product.indexNumber1, Product.indexNumber3, Product.indexNumber3];
+};
+
+Product.renderProducts = function(){
+  Product.chooseThreeImages();
+
+  product1.src = Product.productArray[Product.indexNumber1].src;
+  product2.src = Product.productArray[Product.indexNumber2].src;
+  product3.src = Product.productArray[Product.indexNumber3].src;
+  Product.selectedIndexArray = [Product.indexNumber1, Product.indexNumber2, Product.indexNumber3];
+
+  Product.productArray[Product.indexNumber1].displayCount++;
+  Product.productArray[Product.indexNumber2].displayCount++;
+  Product.productArray[Product.indexNumber3].displayCount++;
+};
+
+/********************************************************************************
+ *         Event Listeners and Handlers                                          *
+ ********************************************************************************/
+productForm.addEventListener('submit', handleVoteSubmit);
+
+function handleVoteSubmit(event) {
+  event.preventDefault();
+  var productForm = document.getElementsByName('productVote');
+
+  for (var i = 0 ; i < productForm.length; i++){
+    if (productForm[i].checked) {
+      var selectedIndex = Product.selectedIndexArray[productForm[i].dataset.index];
+      Product.productArray[selectedIndex].likes++;
+    }
+  }
+  Product.voteCount++;
+
+  if (Product.voteCount === 5){
+    displayResults();
+  } else {
+    Product.renderProducts();
+  }
+}
+
+var displayResults = function() {
+  productForm.style.display = 'none';
+
+  var ulEl = document.createElement
+  productVoteResults.style.display = 
+};
 
 /********************************************************************************
  *         Add instances and call functions for running                          *
@@ -76,19 +115,19 @@ new Product('boots', './images/products/boots.jpg');
 new Product('breakfast', './images/products/breakfast.jpg');
 new Product('bubblegum', './images/products/bubblegum.jpg');
 new Product('chair', './images/products/chair.jpg');
-// new Product('cthulhu', './images/products/cthulhu.jpg');
-// new Product('dog-duck', './images/products/dog-duck.jpg');
-// new Product('dragon', './images/products/dragon.jpg');
-// new Product('pen', './images/products/pen.jpg');
-// new Product('pet-sweep', './images/products/pet-sweep.jpg');
-// new Product('scissors', './images/products/scissors.jpg');
-// new Product('shark', './images/products/shark.jpg');
-// new Product('sweep', './images/products/sweep.png');
-// new Product('tauntaun', './images/products/tauntaun.jpg');
-// new Product('unicorn', './images/products/unicorn.jpg');
-// new Product('usb', './images/products/usb.gif');
-// new Product('water-can', './images/products/water-can.jpg');
-// new Product('wine-glass', './images/products/wine-glass.jpg');
+new Product('cthulhu', './images/products/cthulhu.jpg');
+new Product('dog-duck', './images/products/dog-duck.jpg');
+new Product('dragon', './images/products/dragon.jpg');
+new Product('pen', './images/products/pen.jpg');
+new Product('pet-sweep', './images/products/pet-sweep.jpg');
+new Product('scissors', './images/products/scissors.jpg');
+new Product('shark', './images/products/shark.jpg');
+new Product('sweep', './images/products/sweep.png');
+new Product('tauntaun', './images/products/tauntaun.jpg');
+new Product('unicorn', './images/products/unicorn.jpg');
+new Product('usb', './images/products/usb.gif');
+new Product('water-can', './images/products/water-can.jpg');
+new Product('wine-glass', './images/products/wine-glass.jpg');
 
-Product.renderThreeImages();
+Product.renderProducts();
 
